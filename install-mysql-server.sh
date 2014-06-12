c1830bcc31773986f4e9a74d48447b482979ec82
Daisuke Murase <typester@cpan.org>
2014-06-13 00:35:32 +0900

    hmmm

--- install-mysql-server.sh
+++ install-mysql-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect -f
 set timeout -1
-spawn apt-get install -y mysql-server
+spawn -noecho apt-get install -y mysql-server
 expect "MySQL \"root\" user:"
 send "\r"
 expect "MySQL \"root\" user:"
