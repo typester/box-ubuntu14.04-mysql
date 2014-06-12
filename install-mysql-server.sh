4b702dc2ff8ace678264d1b75d4cf42c3a240ec5
Daisuke Murase <typester@cpan.org>
2014-06-13 00:34:45 +0900

    mmmm

--- install-mysql-server.sh
+++ install-mysql-server.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/expect -f
 set timeout -1
 spawn apt-get install -y mysql-server
-expect "New password for the MySQL \"root\" user:"
+expect "MySQL \"root\" user:"
 send "\r"
-expect "New password for the MySQL \"root\" user:"
+expect "MySQL \"root\" user:"
 send "\r"
-expect "New password for the MySQL \"root\" user:"
+expect "MySQL \"root\" user:"
 send "\r"
 expect eof
-
